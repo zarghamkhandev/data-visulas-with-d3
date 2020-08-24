f77394b917008e774f6b048d39366d2d568a8fe3
zarghamkhandev <zarghamkhan980@gmail.com>
2020-08-24 20:43:53 +0200

    responsiveness added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,5 +8,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  public navbarCollapsed = true;
+
+  changeCollapse(){
+    this.navbarCollapsed = !this.navbarCollapsed
+  }
 
 }
